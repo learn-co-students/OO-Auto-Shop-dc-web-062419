bbaff3ea25fd8ea9be54ecca559cc916453126af
github username <github email address>
2019-07-02 20:51:22 -0400

    Could use some rewording of the tests but the functions are there, so moving on to new problems before cleaning up.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,41 @@
 class Car
 
-  attr_reader :make, :model
+  attr_reader :make, :model, :name, :classification
+  attr_accessor :owner, :mechanic
 
-  def initialize(make, model, classification)
+  @@all = []
+
+  def initialize(name, make, model, classification)
+    @name = name
     @make = make
     @model = model
     @classification = classification
+    self.class.all << self
   end
 
+  # def owner
+  #   @owner = owner
+  # end
+
+  def self.all
+    @@all
+  end
+
+def self.list
+  p self.all.map {|item| item.name }
 end
+
+
+def self.classes
+  p self.all.map {|item| item.classification }
+end
+
+
+def who_best
+  best_mechanic = Mechanic.all.select {|grease_monkey| grease_monkey.specialty == @classification }
+  puts "#{best_mechanic[0].name.to_s} would be the specialist for the car #{self.name} since is classified as #{self.classification}"
+  best_mechanic
+end
+
+end
+
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,46 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    self.class.all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.list_names
+    @@all.collect {|owner| owner.name}
+  end
+
+  def buy(car)
+    car.owner = self
+  end
+
+  def their_cars
+    Car.all.select {|car| car.owner == self}
+  end
+
+  def for_tax_purposes
+    their_cars.length
+  end
+
+  @@total_owned_cars = 0.0 #not sure why this does not show up
+
+  def self.total_owned_cars
+    @@all.each {|owner| @@total_owned_cars += owner.for_tax_purposes}
+    @@total_owned_cars
+  end
+
+  def self.average
+    #assuming some cars may be without individual owners between sales, ownly counting owned cars 
+    # @@total_owned_cars = 0
+    (total_owned_cars/(self.all.length))
+  end
 
 end
+
+
