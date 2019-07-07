32823891e5eea59a9829e9dfbc7614cea3387952
Luke O'Malley <lukeomalley95@gmail.com>
2019-07-07 02:48:23 +0000

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,18 @@
 class Car
 
   attr_reader :make, :model
+  
+  @@all = []
 
   def initialize(make, model, classification)
     @make = make
     @model = model
     @classification = classification
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
   end
 
 end
